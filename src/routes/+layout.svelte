0c786c3a24631b907e2f89c599df2645e1bc485e
Anthony Headley <agheadley@gmail.com>
2024-03-28 13:29:43 +0000

    a16

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,20 @@
 
 
 <div class="app">
+	<nav class="nav">
+		<div class="nav-left">
+		  <a class="brand" href="#">Halite</a>
+		  <div class="tabs">
+			<a>Link 1</a>
+			<a class="active">Link 2</a>
+		  </div>
+		</div>
+		<div class="nav-right">
+		  <a href={'/logout'} class="button outline icon-only">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+		  </a>
+		</div>
+	  </nav>
 	<div class="container">
 		<slot />
 	</div>
---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { onMount } from 'svelte';
+    import * as auth from '$lib/auth';
+    import {location} from '$lib/store'
+    
+    export let data;
+
+    let logout=()=>{
+        console.log('logging out...');
+        auth.logout(data.user.homeAccountId);
+    };
+    
+    onMount(async () => {
+	    console.log($location);
+        console.log(data.user);
+	
+    
+    });
+    
+    </script>
+    
+    <svelte:head>
+        <title>Logout</title>
+        <meta name="description" content="Svelte demo app" />
+    </svelte:head>
+
+    <blockquote>{data.user.name}</blockquote>
+    <div class="row">
+        <div class="col">
+            <p>Do you want to logout?</p>
+            <button class="button dark" on:click={logout}>Logout</button>
+            <a class="button outline" href={data.user.tag.teacher ? $location : '/pupil'}>Cancel</a>
+        </div>
+        <div class="col">
+        </div>
+    </div>
+    <p></p>
+    
+    <style>
+    </style>
