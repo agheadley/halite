182a87b6cdb07afc67de3c38c4feee6fde594758
Anthony Headley <agheadley@gmail.com>
2024-03-31 07:31:46 +0100

    a26

--- src/routes/assessments/create/+page.svelte
+++ src/routes/assessments/create/+page.svelte
@@ -13,11 +13,44 @@
     
 
     };
+
+    let status={
+        list:[],
+        name:'',
+        max:15,
+        dl:'0000-00-00'
+    };
     
+
+   
+    /* at same lv,yr must not have same sc/ss on same date - that is the - so easy to display */
+    let validate=()=>{
+        status.name=status.name.replace(/ /g,"_");
+        status.name=status.name.replace(/[^A-Za-z0-9._-]/g,""); //replace(/\W+/g, "");
+
+        status.name = status.name.length && status.name.length>status.max ? status.name.slice(0,(status.max-1)) : status.name;
+    
+    };
+
+    let save=async()=>{
+
+    };
 
     onMount(async () => {
         $location='/assessments/create';
         console.log(`${$location} mounted`);
+        let y=$cohorts.assessments.years.list[$cohorts.assessments.years.index];
+       
+        let response = await fetch('/edge/distinct', {
+            method: 'POST',
+            body: JSON.stringify({collection:'results',match:{lv:y.lv,yr:y.yr},aggregate:['ss','sc','dl']}),
+            headers: {'content-type': 'application/json'}
+         });
+         status.list=await response.json();
+         //console.log(status.list);
+
+         status.dl=util.getDate();
+
        
     });
     
@@ -26,12 +59,61 @@
     
     <div class="row">
         <div class="col">
+            <h4>
+                Create Assessment 
+                <span class="tag">
+                    {$cohorts.assessments.subjects.list[$cohorts.assessments.subjects.index].sl}
+                    ({$cohorts.assessments.subjects.list[$cohorts.assessments.subjects.index].sc})
+                    {$cohorts.assessments.years.list[$cohorts.assessments.years.index].lv}
+                    {$cohorts.assessments.years.list[$cohorts.assessments.years.index].yr}
+                    
+                </span>
+            </h4>
+        </div>
+        <div class="col">
             <a href={'/assessments'} class="button outline">Close</a>
         </div>
     </div>
     
-    <button class="button error" on:click={test}>test</button>
-    <button class="button" on:click={()=>$alert.msg='back to normal!'}>test</button>
+    <div class="row">
+        <div class="col">
+            &nbsp;
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col is-vertical-align">
+            <fieldset>    
+                <legend>Assessment Name (max {status.max})</legend>
+                    <input type=text bind:value={status.name} class={status.name!=='' ? 'success' : 'error'} on:input={validate}/>
+            </fieldset>
+        </div>
+        <div class="col is-vertical-align">
+            <fieldset>
+                <legend>Assessment Date</legend>
+                <input type=date bind:value={status.dl} class={status.dl!=='' && status.dl!=='0000-00-00' ? 'success' : 'error'}/>
+            </fieldset>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            &nbsp;
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col">
+            <button 
+                disabled={status.name==='' || status.dl==='' || status.dl==='0000-00-00'}
+                class="button dark" 
+                on:click={save}>
+                Save
+                </button>
+    
+        </div>
+    </div>
+    
     
     <style>
 
---
+++ src/routes/edge/distinct/+server.js
@@ -0,0 +1,41 @@
+import { json } from '@sveltejs/kit';
+
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+    runtime: 'edge'
+};
+
+let headers= {
+    'Access-Control-Request-Headers': '*',
+    'Content-Type': 'application/json',
+    'api-key':`${process.env.ATLAS}`
+};
+
+let url = `${process.env.ATLAS_URL}/action/aggregate`;
+
+export async function POST({ request }) {
+   
+    const data = await request.json();
+    
+    
+    /** @type {any} */
+    let distinct={};
+    
+    for(let item of data.aggregate) distinct[item]='$'+item;
+    /** @type {any} */
+    let aggregate=[{$match:data.match},{$group:{_id:distinct}}];
+    //console.log(aggregate);
+    
+    let body = {"collection":data.collection,"database":'halite',"dataSource":process.env.ATLAS_CLUSTER,"pipeline":aggregate};
+    let response = await fetch(url,{method: 'POST',headers: headers,body:JSON.stringify(body)});
+
+    let res=await response.json();
+    console.log('/edge/distinct',res);
+    let results=[];
+    if(res.documents[0]) {
+        for(let item of res.documents ) results.push({...item._id});
+    }
+   
+    console.log('/edge/distinct',results);
+    return json(results);
+}
