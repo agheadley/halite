3cdcee334cea8bb0ae49d21f0098f820a3d9a674
Anthony Headley <agheadley@gmail.com>
2024-05-20 13:28:00 +0100

    a98

--- src/lib/store.js
+++ src/lib/store.js
@@ -27,7 +27,7 @@ export let config=writable({
         
     ],
     overview:[
-        {lv:"",yr:0,exam:false,from:"0000-00-00",to:"0000-00-00",n:"",dl:""}
+        {lv:"",yr:0,exam:false,from:"0000-00-00",to:"0000-00-00",n:"",dl:"",dt:0}
     ]
 
 });
