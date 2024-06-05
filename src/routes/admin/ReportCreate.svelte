a656910d8c067e8d839a8323fddcaedc393319d5
Anthony Headley <agheadley@gmail.com>
2024-06-05 14:19:59 +0100

    a146

--- src/routes/admin/ReportCreate.svelte
+++ src/routes/admin/ReportCreate.svelte
@@ -2,7 +2,7 @@
     import { onMount } from 'svelte';
     import * as file from '$lib/file';
     import * as util from '$lib/util';
-    import {alert,config} from '$lib/store';
+    import {alert,config,groups} from '$lib/store';
     import Modal from '$lib/_Modal.svelte';
 
     /** @type {any}*/
@@ -11,20 +11,21 @@
     /** @type {any}*/
     let data={
        cycle:{},
-       reports:[]
+       reports:[],
+       groups:[]
     };
     
     
 
     let create=async()=>{
-
+        
     };
         
     onMount(async () => {
         console.log('/admin ReportCreate.svelte');
         console.log(status);
 
-       
+        data.cycle={};
         let response = await fetch('/edge/read', {
             method: 'POST',
             body: JSON.stringify({collection:'cycles',filter:{active:true},projection:{}}),
@@ -33,11 +34,35 @@
         let res= await response.json();
         data.cycle=res[0] ? res[0] : {};
 
-       
-       
-       
-    
+        data.reports=[];
+
+        if(data.cycle.active) {
+            response = await fetch('/edge/read', {
+                method: 'POST',
+                body: JSON.stringify({collection:'reports',filter:{coid:data.cycle._id},projection:{}}),
+                headers: {'content-type': 'application/json'}
+            });
+            res= await response.json();
+            data.reports=res[0] ? res : [];
+        }
+
+
+        // copy groups with a filter
+        data.groups=$groups.filter(el=>el.yr>0);
+
+        /* add fm to groups */
+        let d=new Date();
+        let m=d.getMonth()+1;
+        let currentYr=m>$config.rollover.month ? d.getFullYear()+1:d.getFullYear();
+
+        for(let g of data.groups) {
+         
+            let f=$config.year.find((/** @type {{ lv: any; x: number; }} */ el)=>el.lv===g.lv && el.x===(g.yr-currentYr));
+            g.fm = f ? f.fm : -1;
+            //console.log(g.lv,g.yr,g.g,g.fm);
+        }
     });
+     
     
     </script>
 
@@ -53,19 +78,88 @@
     </div>
 
  
-    {#if data.active}
+    {#if data.cycle.active}
     <div class="row">
         <div class="col is-vertical-align">
-            <span class="tag bold">Report Cycle : {data.cycle.term} {data.cycle.subterm} {data.cycle.year}</span>
+            <span class="tag bold">{data.cycle.tt} {data.cycle.ts} {data.cycle.y} ({data.cycle.ay})</span>
         </div> 
     </div>
     <div class="row">
         <div class="col">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Min Length</th>
+                        <th>Max Length</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A</td>
+                        <td>{data.cycle.length.A.min}</td>
+                        <td>{data.cycle.length.A.max}</td>
+                    </tr>
+                    <tr>
+                        <td>E</td>
+                        <td>{data.cycle.length.E.min}</td>
+                        <td>{data.cycle.length.E.max}</td>
+                    </tr>
+                    <tr>
+                        <td>P</td>
+                        <td>{data.cycle.length.P.min}</td>
+                        <td>{data.cycle.length.P.max}</td>
+                    </tr>
+                </tbody>
+                </table>
+
+        </div>
+        <div class="col">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Form</th>
+                        <th>HoD</th>
+                        <th>Teacher</th>
+                        <th>EffortClass</th>
+                        <th>EffortPrep</th>
+                        <th>Enrichment</th>
+                        <th>Tutor</th>
+                        <th>HM</th>
+                        <th>HoS</th>
+                        <th>SLT</th>
+                    </tr>
+                </thead>
+             
+                <tbody>
+                    {#each data.cycle.detail as row,rowIndex}
+                        <tr>
+                            <td>{row.fm}</td>
+                            <td>{row.hod ? 'YES' : ''}</td>
+                            <td>{row.teacher ? 'YES' : ''}</td>
+                            <td>{row.ec ? 'YES' : ''}</td>
+                            <td>{row.ep ? 'YES' : ''}</td>
+                            <td>{row.enrichment ? 'YES' : ''}</td>
+                            <td>{row.tutor ? 'YES' : ''}</td>
+                            <td>{row.hm ? 'YES' : ''}</td>
+                            <td>{row.hoy ? 'YES' : ''}</td>
+                            <td>{row.slt ? 'YES' : ''}</td>
+
+                           
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p>Found {data.reports.length} reports</p>
             {#if data.reports.length && data.reports.length>1}
-            <span class="tag">${data.reports.length} reports already exist, please edit before trying to create.</span>
-            {:else}
-                <button on:click={create} class="button error">Create</button>
+            <p><span class="tag">Please delete existing reports before trying to create.</span></p>
             {/if}
+                <button disabled={data.reports.length>0} on:click={create} class="button error">Create</button>
+           
         </div>
     </div>
     {:else}
